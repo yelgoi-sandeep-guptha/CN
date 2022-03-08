b5b5400223f9fd79f610d4d10422ce3ed7d46002
sandeep <ysandeepguptha3@gmail.com>
2022-03-08 21:52:17 +0530

    Minimum Length Word in the given string

--- M1/Test3/MinLengthWord.java
+++ M1/Test3/MinLengthWord.java
@@ -3,35 +3,36 @@ package M1.Test3;
 public class MinLengthWord {
 
     private static String minLengthWord(String str) {
-        
+
         int minStart = -1;
         int minLength = Integer.MAX_VALUE;
         int currentStart = 0;
         int i = 0;
 
-        for(;i<str.length();i++){
-            if(str.charAt(i)==' '){
-                int currentWordLength = i-currentStart;
-                if(currentWordLength<minLength){
+        for (; i < str.length(); i++) {
+            if (str.charAt(i) == ' ') {
+                int currentWordLength = i - currentStart;
+                if (currentWordLength < minLength) {
                     minStart = currentStart;
                     minLength = currentWordLength;
                 }
-                currentStart = i+1;
+                currentStart = i + 1;
             }
         }
-        if(minStart == -1){
+        if (minStart == -1) {
             return str;
-        }else{
+        } else {
             int currentWordLength = i - currentStart;
-            if(currentWordLength < minLength){
+            if (currentWordLength < minLength) {
                 minStart = currentStart;
                 minLength = currentWordLength;
             }
         }
-        return str.substring(minStart, minStart +minLength);
+        return str.substring(minStart, minStart + minLength);
     }
+
     public static void main(String[] args) {
         String str = "abc de ghihjk a uvw h j";
-        System.out.print("The Minimum length word in the String is : "+minLengthWord(str));
+        System.out.print("The Minimum length word in the String is : " + minLengthWord(str));
     }
 }

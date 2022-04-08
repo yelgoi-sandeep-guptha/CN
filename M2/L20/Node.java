f78f2e918c394dc0781e7e680b0211c0ff4d933f
sandeep <ysandeepguptha3@gmail.com>
2022-04-08 14:34:25 +0530

    Node template

---
+++ M2/L20/Node.java
@@ -0,0 +1,10 @@
+package M2.L20;
+
+public class Node<T> {
+    public T data;
+    public Node<T> next;
+
+    public Node(T data) {
+        this.data = data;
+    }
+}
---
+++ M2/Node.java
@@ -0,0 +1,57 @@
+package M2;
+
+import java.util.Scanner;
+
+public class Node<T> {
+    public T data;
+    public Node<T> next;
+
+    public Node(T data) {
+        this.data = data;
+    }
+
+    public static void printLL(Node<Integer> head) {
+        Node<Integer> temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    /**
+     * takeInput pattern
+     * testcases
+     * enter elements till -1
+     * 
+     */
+
+    public static Node<Integer> takeInput() {
+        Scanner s = new Scanner(System.in);
+        int testCases = s.nextInt();
+        Node<Integer> head = null;
+        while (testCases != 0) {
+            int element = s.nextInt();
+            while (element != -1) {
+                head = addElement(head, element);
+                element = s.nextInt();
+            }
+            testCases--;
+        }
+        s.close();
+        return head;
+    }
+
+    public static Node<Integer> addElement(Node<Integer> head, int data) {
+        Node<Integer> newNode = new Node<Integer>(data);
+        if (head == null) {
+            return newNode;
+        }
+        Node<Integer> temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+        return head;
+    }
+}
